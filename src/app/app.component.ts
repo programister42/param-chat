a78edc762c6230b3f5def769fc576b55014f6f61
Taras Tereschenko <tarrrasio@gmail.com>
2023-09-25 20:48:32 +0500

    Configure linters (#2)
    * feat: change indentation settings in .editorconfig
    
    * feat: add eslint schematics
    
    * feat: add prettier
    
    * refactor: reformat project
    
    * feat: make the linter rules stricter
    
    * refactor: reformat project
    
    * fix: fix images
    
    * feat: add rxjs eslint recommended rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,108 +1,126 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild, afterNextRender, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+	Component,
+	ElementRef,
+	OnInit,
+	Renderer2,
+	ViewChild,
+	afterNextRender,
+	inject,
+	ChangeDetectionStrategy,
+} from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite, Carousel, CarouselItem, CarouselOptions, CarouselInterface } from 'flowbite';
 
 @Component({
-  selector: 'param-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'param-root',
+	standalone: true,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	imports: [CommonModule, RouterOutlet, NgOptimizedImage],
 })
 export class AppComponent implements OnInit {
-  title = 'param-chat';
+	title = 'param-chat';
 
-  @ViewChild('carouselItem1') carouselItem1!: ElementRef;
-  @ViewChild('carouselItem2') carouselItem2!: ElementRef;
-  @ViewChild('carouselItem3') carouselItem3!: ElementRef;
-  @ViewChild('carouselItem4') carouselItem4!: ElementRef;
+	images = [
+		'assets/images/img-1.jpg',
+		'assets/images/img-2.jpg',
+		'assets/images/img-3.jpg',
+		'assets/images/img-4.jpg',
+	];
 
-  @ViewChild('carouselIndicator1') carouselIndicator1!: ElementRef;
-  @ViewChild('carouselIndicator2') carouselIndicator2!: ElementRef;
-  @ViewChild('carouselIndicator3') carouselIndicator3!: ElementRef;
-  @ViewChild('carouselIndicator4') carouselIndicator4!: ElementRef;
+	@ViewChild('carouselItem1') carouselItem1!: ElementRef;
+	@ViewChild('carouselItem2') carouselItem2!: ElementRef;
+	@ViewChild('carouselItem3') carouselItem3!: ElementRef;
+	@ViewChild('carouselItem4') carouselItem4!: ElementRef;
 
-  @ViewChild('dataCarouselPrev') dataCarouselPrev!: ElementRef;
-  @ViewChild('dataCarouselNext') dataCarouselNext!: ElementRef;
+	@ViewChild('carouselIndicator1') carouselIndicator1!: ElementRef;
+	@ViewChild('carouselIndicator2') carouselIndicator2!: ElementRef;
+	@ViewChild('carouselIndicator3') carouselIndicator3!: ElementRef;
+	@ViewChild('carouselIndicator4') carouselIndicator4!: ElementRef;
 
-  private renderer = inject(Renderer2);
+	@ViewChild('dataCarouselPrev') dataCarouselPrev!: ElementRef;
+	@ViewChild('dataCarouselNext') dataCarouselNext!: ElementRef;
 
-  constructor() {
-    afterNextRender(() => {
-      const items: CarouselItem[] = [
-        {
-            position: 0,
-            el: this.carouselItem1.nativeElement
-        },
-        {
-            position: 1,
-            el: this.carouselItem2.nativeElement
-        },
-        {
-            position: 2,
-            el: this.carouselItem3.nativeElement
-        },
-        {
-            position: 3,
-            el: this.carouselItem4.nativeElement
-        },
-      ];
+	private renderer = inject(Renderer2);
 
-      const options: CarouselOptions = {
-        defaultPosition: 1,
-        interval: 3000,
+	constructor() {
+		afterNextRender(() => {
+			const items: CarouselItem[] = [
+				{
+					position: 0,
+					el: this.carouselItem1.nativeElement as HTMLElement,
+				},
+				{
+					position: 1,
+					el: this.carouselItem2.nativeElement as HTMLElement,
+				},
+				{
+					position: 2,
+					el: this.carouselItem3.nativeElement as HTMLElement,
+				},
+				{
+					position: 3,
+					el: this.carouselItem4.nativeElement as HTMLElement,
+				},
+			];
 
-        indicators: {
-            activeClasses: 'bg-white dark:bg-gray-800',
-            inactiveClasses: 'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
-            items: [
-                {
-                    position: 0,
-                    el: this.carouselIndicator1.nativeElement
-                },
-                {
-                    position: 1,
-                    el: this.carouselIndicator2.nativeElement
-                },
-                {
-                    position: 2,
-                    el: this.carouselIndicator3.nativeElement
-                },
-                {
-                    position: 3,
-                    el: this.carouselIndicator4.nativeElement
-                },
-            ]
-        },
+			const options: CarouselOptions = {
+				defaultPosition: 1,
+				interval: 3000,
 
-        // callback functions
-        onNext: () => {
-            console.log('next slider item is shown');
-        },
-        onPrev: ( ) => {
-            console.log('previous slider item is shown');
-        },
-        onChange: ( ) => {
-            console.log('new slider item has been shown');
-        }
-      };
+				indicators: {
+					activeClasses: 'bg-white dark:bg-gray-800',
+					inactiveClasses:
+						'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+					items: [
+						{
+							position: 0,
+							el: this.carouselIndicator1.nativeElement as HTMLElement,
+						},
+						{
+							position: 1,
+							el: this.carouselIndicator2.nativeElement as HTMLElement,
+						},
+						{
+							position: 2,
+							el: this.carouselIndicator3.nativeElement as HTMLElement,
+						},
+						{
+							position: 3,
+							el: this.carouselIndicator4.nativeElement as HTMLElement,
+						},
+					],
+				},
 
-      const carousel: CarouselInterface = new Carousel(items, options);
+				// callback functions
+				onNext: () => {
+					console.log('next slider item is shown');
+				},
+				onPrev: () => {
+					console.log('previous slider item is shown');
+				},
+				onChange: () => {
+					console.log('new slider item has been shown');
+				},
+			};
 
-      carousel.cycle();
+			const carousel: CarouselInterface = new Carousel(items, options);
 
-      this.renderer.listen(this.dataCarouselPrev.nativeElement, 'click', () => {
-        carousel.prev();
-      });
+			carousel.cycle();
 
-      this.renderer.listen(this.dataCarouselNext.nativeElement, 'click', () => {
-        carousel.next();
-      });
-    });
-  }
+			this.renderer.listen(this.dataCarouselPrev.nativeElement, 'click', () => {
+				carousel.prev();
+			});
 
-  ngOnInit() {
-    initFlowbite();
-  }
+			this.renderer.listen(this.dataCarouselNext.nativeElement, 'click', () => {
+				carousel.next();
+			});
+		});
+	}
+
+	ngOnInit() {
+		initFlowbite();
+	}
 }
