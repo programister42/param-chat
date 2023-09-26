94dbd1a51ca369a19339f226c8003790288f0b07
Taras Tereschenko <tarrrasio@gmail.com>
2023-09-26 18:19:13 +0500

    feat: add ngrx store and ngrx-devtools

---
+++ src/app/+state/user/user.reducer.ts
@@ -0,0 +1,29 @@
+import { isDevMode } from '@angular/core';
+import { createFeature, createReducer, MetaReducer, on } from '@ngrx/store';
+import { User } from './user.interface';
+import { userActions } from './user.actions';
+
+interface State {
+	user: User | null;
+}
+
+const initialState: State = {
+	user: null,
+};
+
+export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
+
+export const userFeature = createFeature({
+	name: 'user',
+	reducer: createReducer(
+		initialState,
+		on(userActions.login, (state, user): State => ({ ...state, user })),
+	),
+});
+
+export const {
+	name: userFeatureName,
+	reducer: userReducer,
+	selectUserState,
+	selectUser,
+} = userFeature;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideState, provideStore } from '@ngrx/store';
+import { userFeature } from './+state/user/user.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes)],
+	providers: [
+		provideRouter(routes),
+		provideStore(),
+		provideState(userFeature),
+		provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+	],
 };
