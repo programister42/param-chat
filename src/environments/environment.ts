bef0b9aed97ea9a1e7e59325d1c6edb27caf18c6
Taras Tereschenko <tarrrasio@gmail.com>
2023-09-27 14:07:00 +0500

    integrate-firebase (#10)
    * feat: integrate firebase
    
    * feat: refactor deploy action

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	firebase: {
+		projectId: 'param-chat-2',
+		appId: '1:1058836025056:web:e81b812214a839ca8f8a85',
+		storageBucket: 'param-chat-2.appspot.com',
+		apiKey: 'AIzaSyB4h0QkHVYhs94VHvZwgY0z1dUx59TSaUY',
+		authDomain: 'param-chat-2.firebaseapp.com',
+		messagingSenderId: '1058836025056',
+		measurementId: 'G-PHYFN96B9G',
+	},
+};
