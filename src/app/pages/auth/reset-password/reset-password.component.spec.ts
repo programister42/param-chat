ceb93026f2d8ce639ac673dcf61a302797bef891
Taras Tereschenko <tarrrasio@gmail.com>
2023-10-07 18:28:19 +0500

    refactor: lint project

--- src/app/pages/auth/reset-password/reset-password.component.spec.ts
+++ src/app/pages/auth/reset-password/reset-password.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResetPasswordComponent } from './reset-password.component';
 
 describe('ResetPasswordComponent', () => {
-  let component: ResetPasswordComponent;
-  let fixture: ComponentFixture<ResetPasswordComponent>;
+	let component: ResetPasswordComponent;
+	let fixture: ComponentFixture<ResetPasswordComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ResetPasswordComponent]
-    });
-    fixture = TestBed.createComponent(ResetPasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [ResetPasswordComponent],
+		});
+		fixture = TestBed.createComponent(ResetPasswordComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
