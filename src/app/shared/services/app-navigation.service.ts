a886e531bf804ed9551085e783be555cd7f215d7
Taras Tereschenko <tarrrasio@gmail.com>
2023-10-07 18:22:55 +0500

    feat: add methods to AppNavigationService

--- src/app/shared/services/app-navigation.service.ts
+++ src/app/shared/services/app-navigation.service.ts
@@ -1,3 +1,4 @@
+import { PlatformLocation } from '@angular/common';
 import { Injectable, inject } from '@angular/core';
 import { Router, UrlTree } from '@angular/router';
 
@@ -6,6 +7,9 @@ import { Router, UrlTree } from '@angular/router';
 })
 export class AppNavigationService {
 	private router = inject(Router);
+	private platformLocation = inject(PlatformLocation);
+
+	appProdUrl = 'param-chat.vercel.app';
 
 	get homePageUrlTree(): UrlTree {
 		return this.router.createUrlTree(['/']);
@@ -15,11 +19,23 @@ export class AppNavigationService {
 		return this.router.createUrlTree(['/auth']);
 	}
 
+	get currentAppOriginUrl() {
+		return window.location.origin;
+	}
+
+	get changePasswordPageAbsoluteUrl(): string {
+		return `${this.currentAppOriginUrl}/auth/change-password`;
+	}
+
 	navigateToHomePage(): void {
 		void this.router.navigate(['/']);
 	}
 
 	navigateToAuthPage(): void {
 		void this.router.navigate(['/auth']);
+	}
+
+	navigateToChangePasswordPage(): void {
+		void this.router.navigate(['/auth/change-password']);
 	}
 }
